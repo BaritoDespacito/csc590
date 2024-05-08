b9e44ca21c5444f2a2a98bf86e577799955adacb
BaritoDespacito <vasavathiratham@gmail.com>
2024-05-08 11:29:46 -0400

    nav mesh created

--- Spring 2023-24, Game Dev/feverDream/Assets/Scripts/SkullMove.cs
+++ Spring 2023-24, Game Dev/feverDream/Assets/Scripts/SkullMove.cs
@@ -1,38 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SkullMove : MonoBehaviour
 {
     
-    public Transform Player;
-    int MoveSpeed = 4;
-    int MaxDist = 10;
-    int MinDist = 0;
+    // public Transform Player;
+    // int MoveSpeed = 4;
+    // int MaxDist = 10;
+    // int MinDist = 0;
+    
+    public NavMeshAgent agent;
+    public GameObject player;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(Player);
+        // transform.LookAt(Player);
+        // if (Vector3.Distance(transform.position, Player.position) >= MinDist)
+        // {
+        //     transform.position += MoveSpeed * Time.deltaTime * transform.forward;
+        //     if (Vector3.Distance(transform.position, Player.position) <= MaxDist)
+        //     {
+        //         // Can call function to shoot or dash in or something
+        //     }
+        // }
 
-        if (Vector3.Distance(transform.position, Player.position) >= MinDist)
-        {
-
-            transform.position += MoveSpeed * Time.deltaTime * transform.forward;
-
-
-
-            if (Vector3.Distance(transform.position, Player.position) <= MaxDist)
-            {
-                // Can call function to shoot or dash in or something
-            }
-
-        }
+        agent.SetDestination(player.transform.position);
     }
 }
