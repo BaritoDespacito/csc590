0f40e876b101b7d0f68e9db05c091463ed02ae74
BaritoDespacito <115409559+BaritoDespacito@users.noreply.github.com>
2024-05-14 11:43:50 -0400

    clusters now spawn with distance from player

--- Spring 2023-24, Game Dev/feverDream/Assets/Scripts/EnemyGeneration/Cluster.cs
+++ Spring 2023-24, Game Dev/feverDream/Assets/Scripts/EnemyGeneration/Cluster.cs
@@ -12,10 +12,15 @@ namespace EnemyGeneration
 
         private static double maxDeviation = 10;
         
-        public Cluster(int numEnemies, double maxX, double maxZ)
+        public Cluster(int numEnemies, double maxX, double maxZ, double distFromPlayer, Vector3 playerPos)
         {
             enemies = new ArrayList();
-            position = new Vector3((float) (maxX * UnityEngine.Random.value), 0, (float) (maxZ * UnityEngine.Random.value));
+            position = new Vector3((float) (maxX * UnityEngine.Random.value), 0, (float) (maxX * UnityEngine.Random.value));
+            
+            while (Vector3.Distance(position, playerPos) < distFromPlayer)
+            {
+                position = new Vector3((float) (maxX * UnityEngine.Random.value), 0, (float) (maxX * UnityEngine.Random.value));
+            }
             
             for (var i = 0; i < numEnemies; i++)
             {
--- Spring 2023-24, Game Dev/feverDream/Assets/Scripts/GameControl.cs
+++ Spring 2023-24, Game Dev/feverDream/Assets/Scripts/GameControl.cs
@@ -7,6 +7,7 @@ public class GameControl : MonoBehaviour
     
     public int numClusters = 5;
     public int numEnemies = 10;
+    public double distFromPlayer = 25;
     private ArrayList clusters = new ArrayList();
     
     [SerializeField] private GameObject _enemyPrefab;
@@ -16,11 +17,13 @@ public class GameControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        var player = GameObject.FindWithTag("Player");
+        
         if (!generateEnemies) return;
         
         for (var i = 0; i < numClusters; i++)
         {
-            clusters.Add(new EnemyGeneration.Cluster(numEnemies, 90, 90));
+            clusters.Add(new EnemyGeneration.Cluster(numEnemies, 90, 90, distFromPlayer, player.transform.position));
         }
         
         foreach (EnemyGeneration.Cluster cluster in clusters)
